5c139fe61bc294d637b5b913937a33d0e601e53b
barez <barez@email.com>
2024-02-12 13:36:29 +0300

    This is my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
 
-function App() {
+const skills = [
+  { skill: "Web Design", level: "Professional", color: "#7FFF00" },
+  { skill: "React Js", level: "Intermediate", color: "yellow" },
+  { skill: "OOP", level: "Advanced", color: "#5F9EA0" },
+  { skill: "Graphic Design", level: "Intermediate", color: "skyblue" }
+  
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Info />
+        <SkillList />
+      </div>
     </div>
   );
 }
 
-export default App;
+function Avatar() {
+  return <img src="myPhoto.jpg" alt="Barez" className="avatar" />;
+}
+
+function Info() {
+  return (
+    <React.Fragment>
+      <h1>Barez Zuber Ali </h1>
+
+      <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+    </React.Fragment>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} level={skill.level} color={skill.color} />
+      ))}
+    </div>
+  );
+}
+
+function Skill({ skill, level, color }) {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {(level === "Advanced" && "🚀") ||
+          (level === "Professional" && "😎") ||
+          (level === "Intermediate" && "🤞")}
+      </span>
+    </div>
+  );
+}
+
